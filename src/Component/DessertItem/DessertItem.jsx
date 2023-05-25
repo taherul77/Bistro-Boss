b280bad06608c9ad59738bbdb2eb9aa41c769d00
taherul77 <taherul2317@gmail.com>
2023-05-26 05:46:52 +0600

    add dessert item update

---
+++ src/Component/DessertItem/DessertItem.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import MenuItem from "../PopularMenu/MenuItem/MenuItem";
+
+
+const DessertItem = () => {
+
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+      fetch("menu.json")
+        .then((res) => res.json())
+        .then((data) => {
+          const popularItem = data.filter((item) => item.category === "dessert");
+          setMenu(popularItem);
+        });
+    }, []);
+
+
+    return (
+        <div className="grid grid-cols-12 gap-5 py-8">
+            {
+                menu.map((item) =><MenuItem key={item._id} item={item}></MenuItem>
+                       )
+            }
+        </div>
+    );
+};
+
+export default DessertItem;
--- src/Pages/OurMenu/OurMenu.jsx
+++ src/Pages/OurMenu/OurMenu.jsx
@@ -4,40 +4,41 @@ import image from "../../assets/menu/banner3.jpg";
 import OfferMenu from "../../Component/OfferMenu/OfferMenu";
 import Wrapper from "../../Component/Wrapper/Wrapper";
 import BannerMenu from "../../Component/BannerMenu/BannerMenu";
-import menuImage from "../../assets/home/chef-service.jpg"
-
+import menuImage from "../../assets/home/chef-service.jpg";
+import DessertItem from "../../Component/DessertItem/DessertItem";
 
 const OurMenu = () => {
-    useTitle('Our Menu');
-    return (
+  useTitle("Our Menu");
+  return (
+    <div>
+      <MenuBanner
+        img={image}
+        heading={"OUR MENU"}
+        description={"WOULD YOU LIKE TO TRY A DISH ?"}
+      ></MenuBanner>
+
+      <Wrapper className="py-6">
+        <OfferMenu></OfferMenu>
+      </Wrapper>
+      <div>
+        <BannerMenu
+          img={menuImage}
+          heading={"DESSERTS"}
+          description={
+            "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          }
+        ></BannerMenu>
         <div>
-            <MenuBanner
-            img={image}
-            heading={"OUR MENU"}
-            description={
-                "WOULD YOU LIKE TO TRY A DISH ?"
-            }
-            
-            ></MenuBanner>
+            <Wrapper>
+                
+            <DessertItem></DessertItem>
 
-           <Wrapper className='py-6'>
-           <OfferMenu></OfferMenu>
-           </Wrapper>
-           <BannerMenu
-           
-           img={menuImage}
-           heading={"DESSERTS"}
-           description={
-               "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-           }
-           
-           >
-
-           </BannerMenu>
-
-            
+            </Wrapper>
+        
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default OurMenu;
