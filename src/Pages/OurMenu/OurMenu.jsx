9b025261cfc1318e0f80de311754df341a485018
taherul77 <taherul2317@gmail.com>
2023-05-26 04:59:31 +0600

    add dessert banner

--- src/Pages/OurMenu/OurMenu.jsx
+++ src/Pages/OurMenu/OurMenu.jsx
@@ -3,6 +3,8 @@ import useTitle from "../../hooks/useTitle";
 import image from "../../assets/menu/banner3.jpg";
 import OfferMenu from "../../Component/OfferMenu/OfferMenu";
 import Wrapper from "../../Component/Wrapper/Wrapper";
+import BannerMenu from "../../Component/BannerMenu/BannerMenu";
+import menuImage from "../../assets/home/chef-service.jpg"
 
 
 const OurMenu = () => {
@@ -21,6 +23,17 @@ const OurMenu = () => {
            <Wrapper className='py-6'>
            <OfferMenu></OfferMenu>
            </Wrapper>
+           <BannerMenu
+           
+           img={menuImage}
+           heading={"DESSERTS"}
+           description={
+               "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+           }
+           
+           >
+
+           </BannerMenu>
 
             
         </div>
