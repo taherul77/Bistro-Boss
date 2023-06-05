d40cc1ed34bde5c7fe8b890e6adfa200ce6fa7cb
taherul77 <taherul2317@gmail.com>
2023-06-05 17:29:12 +0600

    update

--- src/Component/Dashboard/Dashboard.jsx
+++ src/Component/Dashboard/Dashboard.jsx
@@ -12,7 +12,7 @@ const Dashboard = () => {
   return (
     <div className="drawer lg:drawer-open">
     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content flex flex-col mx-auto  items-center justify-center">
+    <div className="drawer-content w-full flex flex-col mx-auto  items-center justify-center">
     <label htmlFor="my-drawer-2" className="btn bg-[#D1A054] drawer-button lg:hidden">Open drawer</label>
       <Outlet></Outlet>
       
@@ -20,7 +20,7 @@ const Dashboard = () => {
     </div> 
     <div className="drawer-side">
       <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-      <ul className="menu p-4 w-80 text-center text-xl h-full bg-[#D1A054] text-base-content">
+      <ul className="menu p-4 w-80 text-center text-xl h-[100%] bg-[#D1A054] text-base-content">
         {/* Sidebar content here */}
 
         <h2 className="text-4xl py-6">Bistro Boss</h2>
